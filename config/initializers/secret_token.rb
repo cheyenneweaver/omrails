5fdd3042b5eb5fdd21adcc1447a67f8d36e1721d
cheyenneweaver <cheyenneweaver@gmail.com>
2012-12-05 11:33:33 -0600

    After doing all the videos
    for One Month Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '907b29a45c50ff9406ddc98fad821ff2925b6cb434a64180bbbd926ad6b54ebd972ab2484a3a9afe1f499c5013bf0935664c15820f0a3e0f98ebe2e0b9be511e'
